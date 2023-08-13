29c6468bae84e277c74c7f426d4e0e4d49f74f0d
kyscodeman <metalgos2@gmail.com>
2023-08-14 02:46:45 +0900

    프로젝트 컨텍스트 패스 수정 전

---
+++ webprj/src/main/java/com/newlecture/web/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.newlecture.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class IndexController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		
+		System.out.println("index controoler in ");
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("data", "Welcome to Spring");
+		mv.setViewName("/WEB-INF/view/index.jsp");
+
+		
+		return mv;
+	}
+
+}
