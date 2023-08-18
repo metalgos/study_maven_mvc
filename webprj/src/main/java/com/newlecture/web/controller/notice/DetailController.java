3141a3bcad63552be7fd8804e7d271477d502709
kyscodeman <metalgos2@gmail.com>
2023-08-19 00:10:34 +0900

    타일즈 기본 xml설정1

--- webprj/src/main/java/com/newlecture/web/controller/notice/DetailController.java
+++ webprj/src/main/java/com/newlecture/web/controller/notice/DetailController.java
@@ -11,7 +11,7 @@ public class DetailController implements Controller {
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		System.out.println("DetailControoler in ");
-		ModelAndView mv = new ModelAndView("notice/detail");//실제로 찾아갈 주소
+		ModelAndView mv = new ModelAndView("notice.detail");//실제로 찾아갈 주소
 		
 		//mv.setViewName("/WEB-INF/view/index.jsp");
 		
--- webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
+++ webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
@@ -11,7 +11,7 @@ public class ListController implements Controller {
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		System.out.println("ListControoler in ");
-		ModelAndView mv = new ModelAndView("notice/list");//실제로 찾아갈 주소
+		ModelAndView mv = new ModelAndView("notice.list");//실제로 찾아갈 주소
 		
 		//mv.setViewName("/WEB-INF/view/index.jsp");
 		
