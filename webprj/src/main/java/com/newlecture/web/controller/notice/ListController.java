086f60afb6f398322ab23d5c5c72386092e7fee9
kyscodeman <metalgos2@gmail.com>
2023-09-01 23:47:12 +0900

    수정중

--- webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
+++ webprj/src/main/java/com/newlecture/web/controller/notice/ListController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
@@ -19,7 +20,7 @@ public class ListController implements Controller {
 	
 	
 	
-
+@Autowired
 	public void setNoticeService(JDBCNoticeService noticeService) {
 		this.noticeService = noticeService;
 	}
