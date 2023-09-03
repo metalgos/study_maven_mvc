0cd569728078d29fe3cdd880614afc440f6d28c8
kyscodeman <metalgos2@gmail.com>
2023-09-03 09:52:23 +0900

    리스폰스 바디 예제

--- webprj/src/main/java/com/newlecture/web/controller/HomeController.java
+++ webprj/src/main/java/com/newlecture/web/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.newlecture.web.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @RequestMapping("/")
@@ -9,8 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 	
 	@RequestMapping("index")
-	public String index() {
-		return "root.index";
+	@ResponseBody
+	public String index(HttpServletResponse response) {
+		
+				
+		return "hello Index 1";
 	}
 
 	
