589e4d56f70af3e9867179ff12f5e25238b1cc3d
kyscodeman <metalgos2@gmail.com>
2023-09-06 03:28:06 +0900

    폼 post 입력

--- webprj/src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
+++ webprj/src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
@@ -18,9 +18,9 @@ public class NoticeController {
 
 	@RequestMapping("reg")
 	@ResponseBody
-	public String reg() {
+	public String reg(String title, String content) {
 
-		return "reg";
+		return String.format("title :%s, <br> content : %s <br",title,content);
 	}
 
 
