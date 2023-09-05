ea300247f4c8ca04ca7b8fe2b49b8e5867a099ed
kyscodeman <metalgos2@gmail.com>
2023-09-06 02:13:09 +0900

    리퀘스트 파람 어노테이션 이용

--- webprj/src/main/java/com/newlecture/web/controller/customer/NoticeController.java
+++ webprj/src/main/java/com/newlecture/web/controller/customer/NoticeController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.newlecture.web.entity.Notice;
 import com.newlecture.web.service.NoticeService;
@@ -20,10 +21,10 @@ public class NoticeController {
 	private NoticeService noticeService;
 	
 	@RequestMapping("/list")
-	public String list(String p) throws ClassNotFoundException, SQLException {
+	public String list(@RequestParam(name = "p", defaultValue = "1") String page) throws ClassNotFoundException, SQLException {
 		
 		//String p = request.getParameter("p");
-		System.out.println("page : "+p);
+		System.out.println("page : "+page);
 		
 		//List<Notice> list = service.getList(1, "title", "");
 		
